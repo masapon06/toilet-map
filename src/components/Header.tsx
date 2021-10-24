6b8aca1d0915b5f8db778cca230791ca31534314
masapon0618 <masapon0618@gmail.com>
2021-10-24 17:06:55 +0900

    update headerのcssをstyled-componentに

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import { ScreenType } from '../valueobject/Screen';
+import React from 'react'
+import styled from 'styled-components'
+import { ScreenType } from '../valueobject/Screen'
+import { TitleStyle } from './Styles'
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -11,8 +12,129 @@ const HeaderWrapper = styled.div`
   position: fixed;
   top: 0;
   background-color: #ffffff;
-`;
+`
 
+const ContentWrapper = styled.div`
+  overflow: hidden;
+  margin-top: 0;
+  width: 100vw;
+`
+
+const TitleWrapper = styled.div`
+  color: #0080c9;
+  height: 35px;
+  flex-shrink: 0;
+  display: flex;
+  position: relative;
+  -webkit-box-align: center;
+  align-items: center;
+`
+
+const SwitchWrapper = styled.div`
+  height: 29px;
+  min-height: 29px;
+  border-width: 1px;
+  border-color: #0080c9;
+  border-radius: 5px;
+  border-style: solid;
+  display: flex;
+  justify-content: stretch;
+  -webkit-box-align: stretch;
+  align-items: stretch;
+  margin: 0px 16px 16px;
+`
+
+const MapOffButton = styled.div`
+  min-width: 1px;
+  width: 1px;
+  -webkit-box-flex: 1;
+  flex-grow: 1;
+  flex-shrink: 1;
+  color: #0080c9;
+  background-color: transparent;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.0625rem;
+  font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+  text-align: center;
+  text-transform: lowercase;
+  font-variant: small-caps;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  transition: background-color 0.2s ease 0s, color 0.2s ease 0s;
+`
+
+const ListOnButton = styled.div`
+  min-width: 1px;
+  width: 1px;
+  -webkit-box-flex: 1;
+  flex-grow: 1;
+  flex-shrink: 1;
+  color: rgba(255, 255, 255, 0.976);
+  background-color: #0080c9;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.0625rem;
+  font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+  text-align: center;
+  text-transform: lowercase;
+  font-variant: small-caps;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  transition: background-color 0.2s ease 0s, color 0.2s ease 0s;
+`
+
+const MapOnButton = styled.div`
+  min-width: 1px;
+  width: 1px;
+  -webkit-box-flex: 1;
+  flex-grow: 1;
+  flex-shrink: 1;
+  color: rgba(255, 255, 255, 0.976);
+  background-color: #0080c9;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.0625rem;
+  font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+  text-align: center;
+  text-transform: lowercase;
+  font-variant: small-caps;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  transition: background-color 0.2s ease 0s, color 0.2s ease 0s;
+`
+
+const ListOffButton = styled.div`
+  min-width: 1px;
+  width: 1px;
+  -webkit-box-flex: 1;
+  flex-grow: 1;
+  flex-shrink: 1;
+  color: #0080c9;
+  background-color: transparent;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.0625rem;
+  font-family: -apple-system, BlinkMacSystemFont, Roboto, sans-serif;
+  text-align: center;
+  text-transform: lowercase;
+  font-variant: small-caps;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  transition: background-color 0.2s ease 0s, color 0.2s ease 0s;
+`
 interface HeaderProps {
   switchScreen: ScreenType
   onClickMapButton: () => void
@@ -24,31 +146,29 @@ export const Header: React.FC<HeaderProps> = ({
   onClickMapButton,
   onClickListButton,
 }) => {
-    return (
-        <HeaderWrapper>
-          <div className="header-wrapper">
-            <div className="title-wrapper">
-              <div className="title">
-                目的地で検索
-              </div>
-            </div>
-            <div className="all-wrapper">
-              <div className="switch-wrapper">
-                { switchScreen.isVisibleList &&
-                <div className="map-off-button" onClick={() => onClickMapButton()}>マップで表示</div>
-                }
-                { switchScreen.isVisibleList && 
-                <div className="list-on-button">リストで表示</div>
-                }
-                { switchScreen.isVisibleMap &&
-                <div className="map-on-button">マップで表示</div>
-                }
-                { switchScreen.isVisibleMap && 
-                <div className="list-off-button" onClick={() => onClickListButton()}>リストで表示</div>
-                }
-              </div>
-            </div>
-          </div>
-        </HeaderWrapper>
-    )
-};
+  return (
+    <HeaderWrapper>
+      <ContentWrapper>
+        <TitleWrapper>
+          <TitleStyle>目的地で検索</TitleStyle>
+        </TitleWrapper>
+        <SwitchWrapper>
+          {switchScreen.isVisibleList && (
+            <MapOffButton onClick={() => onClickMapButton()}>
+              マップで表示
+            </MapOffButton>
+          )}
+          {switchScreen.isVisibleList && (
+            <ListOnButton>リストで表示</ListOnButton>
+          )}
+          {switchScreen.isVisibleMap && <MapOnButton>マップで表示</MapOnButton>}
+          {switchScreen.isVisibleMap && (
+            <ListOffButton onClick={() => onClickListButton()}>
+              リストで表示
+            </ListOffButton>
+          )}
+        </SwitchWrapper>
+      </ContentWrapper>
+    </HeaderWrapper>
+  )
+}
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@mui/styles';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLng } from "leaflet";
-import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-import { PlaceType } from '../entity/Place';
- 
+import React from 'react'
+import { makeStyles } from '@mui/styles'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { LatLng } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { PlaceType } from '../entity/Place'
+
 const placeholder = L.icon({
   iconUrl: 'placeholder.png',
   iconSize: [40, 40],
@@ -13,47 +13,53 @@ const placeholder = L.icon({
   popupAnchor: [7, -40],
   // shadowUrl: '必要であれば'
   shadowSize: [30, 60],
-  shadowAnchor: [8, 60]
-});
+  shadowAnchor: [8, 60],
+})
 
 const useStyles = makeStyles({
-  root: {
-    textAlign: 'center',
-  },
+  root: {},
   leafletContainer: {
     width: '100vw',
-    height: '81vh',
-  }
-});
+    height: '71vh',
+  },
+})
 
 interface MapProps {
   places: PlaceType[]
 }
 
-export const Map: React.FC<MapProps> = props => {
+export const Map: React.FC<MapProps> = (props) => {
   const { places } = props
 
   const classes = useStyles()
 
-  const position = new LatLng(37.9120388, 139.0595863);
+  const position = new LatLng(37.9120388, 139.0595863)
   return (
     <div className={classes.root}>
-      <MapContainer center={position} zoom={13} zoomControl={false}className={classes.leafletContainer}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        zoomControl={false}
+        className={classes.leafletContainer}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {places.map(place => (
-          <Marker position={new LatLng(place.latitude, place.longitude)} icon={placeholder}>
-          <Popup>
-            ここから{place.distance}km
-            <br/><br/>
-            {place.placeName}
-          </Popup>
+        {places.map((place) => (
+          <Marker
+            position={new LatLng(place.latitude, place.longitude)}
+            icon={placeholder}
+          >
+            <Popup>
+              ここから{place.distance}km
+              <br />
+              <br />
+              {place.placeName}
+            </Popup>
           </Marker>
-        ))
-        }
+        ))}
       </MapContainer>
     </div>
-  );
+  )
 }
